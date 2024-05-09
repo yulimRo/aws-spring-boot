ef71a464698c4b5a9964455ce6221005e79e2812
ylro <ylro@mcnc>
2024-05-09 14:16:23 +0900

    init commit

---
+++ health-gard/src/main/java/com/healthgard/server/HealthGardApplication.java
@@ -0,0 +1,13 @@
+package com.healthgard.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthGardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthGardApplication.class, args);
+	}
+
+}
